7aabc430551a08bd860d234aed079ff2a4e61279
Lucas Suppes <supperino@users.noreply.github.com>
2017-08-28 20:31:40 -0300

    herança basica de classes

--- painthon.py
+++ painthon.py
@@ -1,23 +1,23 @@
 from tkinter import *
 
-class tools:
+class tool:
     def __init__(self, Canvas=None):
-        self.Canvas = Canvas
+        #self.Canvas = Canvas
         self.left_but = "up"
         self.x_pos, self.y_pos = None, None
 
     def left_but_down(self, event=None):
         self.left_but = "down"
-        print(event.x, event.y)
  
     def left_but_up(self, event=None):
         self.left_but = "up"
         self.x_pos = None
         self.y_pos = None
- 
+
+class brush(tool):
     def motion(self, event=None):
             self.pencil_draw(event)
- 
+
     def pencil_draw(self, event=None):
         if self.left_but == "down":
             if self.x_pos is not None and self.y_pos is not None:
@@ -29,10 +29,11 @@ class paint:
     def __init__(self, root):
         drawing_area = Canvas(root)
         drawing_area.pack()
-        self.tools = tools(drawing_area)
-        drawing_area.bind("<Motion>", self.tools.motion)
-        drawing_area.bind("<ButtonPress-1>", self.tools.left_but_down)
-        drawing_area.bind("<ButtonRelease-1>", self.tools.left_but_up)  
+        self.tool = tool(drawing_area)
+        self.brush = brush(drawing_area)
+        drawing_area.bind("<Motion>", self.brush.motion)
+        drawing_area.bind("<ButtonPress-1>", self.brush.left_but_down)
+        drawing_area.bind("<ButtonRelease-1>", self.brush.left_but_up)  
 
 root = Tk()
 paint_application = paint(root)
