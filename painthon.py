b7e104f1989f9c320526ad7d7cc6836a1414cfd7
Lucas Suppes <supperino@users.noreply.github.com>
2017-08-28 21:30:18 -0300

    class to draw rectangles (still, very ugly) but functional
    Славься, Отечество наше свободное,
    
    Братских народов союз вековой,
    
    Предками данная мудрость народная!
    
    Славься, страна! Мы гордимся тобой!

--- painthon.py
+++ painthon.py
@@ -1,18 +1,23 @@
 from tkinter import *
 
 class tool:
-    def __init__(self, Canvas=None):
         #self.Canvas = Canvas
+    def __init__(self, Canvas=None):
+        self.x1_line_pt, self.y1_line_pt = None, None
+        self.x2_line_pt, self.y2_line_pt = None, None
         self.left_but = "up"
         self.x_pos, self.y_pos = None, None
 
     def left_but_down(self, event=None):
         self.left_but = "down"
+        self.x1_line_pt = event.x
+        self.y1_line_pt = event.y
  
     def left_but_up(self, event=None):
         self.left_but = "up"
-        self.x_pos = None
-        self.y_pos = None
+        self.x_pos,self.y_pos = None, None
+        self.x2_line_pt = event.x
+        self.y2_line_pt = event.y
 
 class brush(tool):
     def motion(self, event=None):
@@ -25,16 +30,44 @@ class brush(tool):
             self.x_pos = event.x
             self.y_pos = event.y
 
+class rectangle(tool):
+    def left_but_down(self, event=None):
+        self.left_but = "down"
+        self.x1_line_pt = event.x
+        self.y1_line_pt = event.y
+ 
+    def left_but_up(self, event=None):
+        self.left_but = "up"
+        self.x_pos,self.y_pos = None, None
+        self.x2_line_pt = event.x
+        self.y2_line_pt = event.y
+        self.rectangle_draw(event)
+
+    def rectangle_draw(self, event=None):
+        if None not in (self.x1_line_pt, self.y1_line_pt, self.x2_line_pt,
+        self.y2_line_pt):
+            event.widget.create_rectangle(self.x1_line_pt, self.y1_line_pt,
+            self.x2_line_pt, self.y2_line_pt,fill="midnight blue",outline="yellow",
+            width=2)
+
 class paint:
     def __init__(self, root):
-        drawing_area = Canvas(root)
-        drawing_area.pack()
-        self.tool = tool(drawing_area)
-        self.brush = brush(drawing_area)
-        drawing_area.bind("<Motion>", self.brush.motion)
-        drawing_area.bind("<ButtonPress-1>", self.brush.left_but_down)
-        drawing_area.bind("<ButtonRelease-1>", self.brush.left_but_up)  
+        self.drawing_area = Canvas(root)
+        self.drawing_area.pack()
+        self.deftool = "rectangle"
+        self.tool = tool(self.drawing_area)
+        self.select_tool()
+
+    def select_tool(self):
+        if self.deftool == "rectangle" :
+            self.tool = rectangle(self.drawing_area)
+        else :
+            self.tool = brush(self.drawing_area)
+            self.drawing_area.bind("<Motion>", self.tool.motion)
+
+        self.drawing_area.bind("<ButtonPress-1>", self.tool.left_but_down)
+        self.drawing_area.bind("<ButtonRelease-1>", self.tool.left_but_up)
 
 root = Tk()
 paint_application = paint(root)
-mainloop()
+mainloop(
